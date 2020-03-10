ce17de94d0be3422746e78d68ca95c8292a09657
MansAhmadBONgit config --global user.name MansAhmadBONgit config --global user.name MansAhmadBON <mansue.ahmad.94@mail.ru>
2020-03-10 23:48:41 +0300

    prepared the project for deployment

---
+++ sourse/src/js/main.js
@@ -0,0 +1,98 @@
+window.onload = function () {
+    const wrapBtnsPhotos = document.querySelector('.galleryMain__btns');
+    const arrayBtnsPhotos = document.querySelectorAll('.galleryMain__btn');
+
+    const galleryPhotos = document.querySelectorAll('.galleryPhotos');
+    const photosFam = document.querySelector('.galleryPhotos-fam');
+    const photosLove = document.querySelector('.galleryPhotos-love');
+    const photosLife = document.querySelector('.galleryPhotos-life');
+    const photosGlam = document.querySelector('.galleryPhotos-glam');
+    const photosFashion = document.querySelector('.galleryPhotos-fashion');
+
+
+    const btnPlayVideo = document.querySelector('.videoPoster__btn');
+    const btnCloseVideo = document.querySelector('.video__close');
+    const wrapperVideo = document.querySelector('.wrapperVideo');
+    const videoPoster = document.querySelector('.videoPoster');
+
+
+
+    function removeClassesGallerey(){
+        galleryPhotos.forEach(gallery => {
+            arrayBtnsPhotos.forEach(btn => btn.classList.remove('galleryMain__btn-active'));
+            gallery.classList.remove('galleryPhotos-activeFlex');
+            gallery.classList.remove('galleryPhotos-active')
+        });
+    }
+
+
+    //tabs
+    wrapBtnsPhotos.addEventListener('click', function (e) {
+        let btnClassName = e.target.className.split(' ')[1];
+
+        switch (btnClassName) {
+            case 'galleryMain__btn-fam':
+                removeClassesGallerey();
+
+                photosFam.classList.add('galleryPhotos-activeFlex');
+                e.target.classList.add('galleryMain__btn-active');
+                break;
+
+            case 'galleryMain__btn-love':
+                removeClassesGallerey();
+
+                photosLove.classList.add('galleryPhotos-activeFlex');
+                e.target.classList.add('galleryMain__btn-active');
+                break;
+
+            case 'galleryMain__btn-life':
+                removeClassesGallerey();
+
+                photosLife.classList.add('galleryPhotos-activeFlex');
+                e.target.classList.add('galleryMain__btn-active');
+                break;
+
+            case 'galleryMain__btn-glam':
+                removeClassesGallerey();
+
+                photosGlam.classList.add('galleryPhotos-activeFlex');
+                e.target.classList.add('galleryMain__btn-active');
+                break;
+
+            case 'galleryMain__btn-fashion':
+                removeClassesGallerey();
+
+                photosFashion.classList.add('galleryPhotos-active');
+                e.target.classList.add('galleryMain__btn-active');
+                break;
+
+            default: break;
+        }
+    });
+
+
+
+    btnPlayVideo.addEventListener('click', function (e) {
+        wrapperVideo.classList.add('wrapperVideo-active');
+        videoPoster.classList.remove('videoPoster-active');
+    });
+
+    btnCloseVideo.addEventListener('click', function () {
+        wrapperVideo.classList.remove('wrapperVideo-active');
+        videoPoster.classList.add('videoPoster-active');
+    });
+};
+
+//параллакс
+$(window).scroll(function(){
+    let scrollTop = $(this).scrollTop();
+    $('.hero').css({ 'background-position-y': scrollTop / 2 + 'px'})
+});
+
+//скролл до section
+$('a[data-target^="anchor"]').bind('click.smoothscroll', function(){
+    const target = $(this).attr('href');
+    let ofSetTop = $(target).offset().top;
+    $('body, html').animate({scrollTop: ofSetTop}, 700);
+    return false;
+});
